adc8b9bfa5468bfa649a6b630767126a20d173aa
Robert Tam <rsh.tam@gmail.com>
2019-08-13 15:45:58 +0100

    mount added

--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -2,7 +2,29 @@ import React, {Component} from 'react';
 import MusicSelector from '../components/MusicSelector';
 import MusicDetail from '../components/MusicDetail';
 
+
+
 class MusicBox extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    songs: [],
+    currentSong: null
+  };
+  this.handleSongSelected = this.handleSongSelected.bind(this);
+}
+
+componentDidMount() {
+  const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
+  fetch(url)
+  .then(res=>res.json())
+  .then(songs => this.setState({songs: songs}))
+  .catch(err => console.error);
+}
+
+
+
   render() {
     return(
       <div>
