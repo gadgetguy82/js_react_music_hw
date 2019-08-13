36bf3fda9f9578302981bf85fe079424d754ff73
Robert Tam <rsh.tam@gmail.com>
2019-08-13 18:24:33 +0100

    Add more info - image and audio preview plus some basic css

--- src/components/MusicDetail.js
+++ src/components/MusicDetail.js
@@ -1,13 +1,22 @@
-import React, {Fragment} from 'react';
+import React from 'react';
+import './MusicDetail.css'
 
 const MusicDetail = (props) => {
   if (!props.song) return null;
   return(
-    <Fragment>
-    <h3>{props.song['im:name'].label}</h3>
-    <p> Artist: {props.song['im:artist'].label} </p>
-    <p> Chart Position: {props.position}</p>
-    </Fragment>
+    <div className="music-detail">
+      <div className="info-image">
+        <div className="info">
+          <h3>{props.song['im:name'].label}</h3>
+          <p> Artist: {props.song['im:artist'].label} </p>
+          <p> Chart Position: {props.position}</p>
+        </div>
+        <img src={props.song['im:image'][2].label} alt="" />
+      </div>
+      <audio controls>
+        <source src={props.song.link[1].attributes.href} />
+      </audio>
+    </div>
   )
 }
 
--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -2,36 +2,34 @@ import React, {Component} from 'react';
 import MusicSelector from '../components/MusicSelector';
 import MusicDetail from '../components/MusicDetail';
 
-
-
 class MusicBox extends Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    songs: [],
-    currentSong: null,
-    position: 0
-  };
-  this.handleSongSelected = this.handleSongSelected.bind(this);
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+      currentSong: null,
+      position: 0
+    };
+    this.handleSongSelected = this.handleSongSelected.bind(this);
+  }
 
-componentDidMount() {
-  const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
-  fetch(url)
-  .then(res=>res.json())
-  .then(songs => this.setState({songs: songs.feed.entry}))
-  .catch(err => console.error);
-}
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
+    fetch(url)
+    .then(res=>res.json())
+    .then(songs => this.setState({songs: songs.feed.entry}))
+    .catch(err => console.error);
+  }
 
-handleSongSelected(index) {
-  const selectedSong = this.state.songs[index];
-  this.setState({currentSong: selectedSong, position: parseInt(index) + 1});
-}
+  handleSongSelected(index) {
+    const selectedSong = this.state.songs[index];
+    this.setState({currentSong: selectedSong, position: parseInt(index) + 1});
+  }
 
   render() {
     return(
-      <div>
+      <div className="music-box">
         <MusicSelector
         songs={this.state.songs}
         onSongSelected={this.handleSongSelected}
